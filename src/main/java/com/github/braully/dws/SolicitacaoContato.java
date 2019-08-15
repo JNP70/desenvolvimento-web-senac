69178e08f0cb1fd48e5649751dbaaa61a33b2243
Aluno <Aluno@PC-43682>
2019-08-14 21:46:51 -0300

    Aula Tres

---
+++ src/main/java/com/github/braully/dws/SolicitacaoContato.java
@@ -0,0 +1,18 @@
+
+package com.github.braully.dws;
+
+
+public class SolicitacaoContato {
+
+     String nome;
+     String email;
+     String duvida;
+
+    @Override
+    public String toString() {
+        return "SolicitacaoContato{" + "nome=" + nome + ", email=" + email + ", duvida=" + duvida + '}';
+    }
+
+
+    
+}
